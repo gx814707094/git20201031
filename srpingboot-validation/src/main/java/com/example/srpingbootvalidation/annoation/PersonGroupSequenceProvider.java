ced8409cc92a3318ea33ef5026443aeae8d35c74
wwcc <1351786511@qq.com>
2020-10-31 17:23:13 +0800

    王聪1101代码提交----2

--- srpingboot-validation/src/main/java/com/example/srpingbootvalidation/annoation/PersonGroupSequenceProvider.java
+++ srpingboot-validation/src/main/java/com/example/srpingbootvalidation/annoation/PersonGroupSequenceProvider.java
@@ -3,9 +3,6 @@ package com.example.srpingbootvalidation.annoation;
 import com.example.srpingbootvalidation.bean.Person;
 import org.hibernate.validator.spi.group.DefaultGroupSequenceProvider;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,14 +19,27 @@ public class PersonGroupSequenceProvider implements DefaultGroupSequenceProvider
         List<Class<?>> defaultGroupSequence = new ArrayList<>();
         //默认组
         defaultGroupSequence.add(Person.class);
-
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
+        System.out.println("我是加王佛那个");
         //根据年龄进行组选择
         if(person != null){
           Integer age = person.getAge();
           if(age < 5){
-              defaultGroupSequence.add(Person.lev1.class);
+//              defaultGroupSequence.add(Person.lev1.class);
           }else if(age < 18){
-              defaultGroupSequence.add(Person.lev2.class);
+//              defaultGroupSequence.add(Person.lev2.class);
           }else{
               defaultGroupSequence.add(Person.lev3.class);
           }
