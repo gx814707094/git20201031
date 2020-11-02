76140ecc14e16da7bb744fcb5c65df9823552b9c
gengxiao <gx814707094@163.com>
2020-11-02 22:07:51 +0800

    新建

---
+++ srpingboot-validation/src/main/java/com/example/srpingbootvalidation/controller/TestController3.java
@@ -0,0 +1,10 @@
+package com.example.srpingbootvalidation.controller;
+
+/**
+ * @Classname TestController3
+ * @Description
+ * @Date 2020/11/2 22:07
+ * @Created gengxiao
+ */
+public class TestController3 {
+}
